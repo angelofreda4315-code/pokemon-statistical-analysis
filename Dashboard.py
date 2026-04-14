ba9652ac3a2290630e777e3a8b606d3503a93612
angelo <angelofreda4315@gmail.com>
2026-04-14 17:48:30 -0400

    modifico grafico de caja y bigote para que legendarios y no legendarios se vean por se parados y coloco interpretaciones de cada cosa

--- Dashboard.py
+++ Dashboard.py
@@ -42,8 +42,9 @@ st.title("📈 ¿Los Pokémon legendarios son realmente superiores?")
 #----------------Pestañas----------------
 tab1, tab2, tab3 = st.tabs(["📊Comparación general", "🏃Análisis de velocidad", "💪Fuerza normal y legendaria"])
 
-
+#------------------------------------------------------
 #-----------------------pestaña 1----------------------
+#------------------------------------------------------
 with tab1:
     #--------------primer punto----------------
     if len(filtrados) > 0:
@@ -56,6 +57,8 @@ with tab1:
     col1, col2, col3 = st.columns(3)
     col1.metric("Promedio Legendarios", f"{prom_leyenda:.1f}" if len(filtrados)>0 else "Sin datos")
     col2.metric("Promedio Normales", f"{prom_normal:.1f}" if len(filtrados)>0 else "Sin datos")
+    
+    st.markdown("📌 Conclusión: En promedio, los Pokémon legendarios tienen una fuerza total significativamente mayor que los normales. La diferencia porcentual muestra que los legendarios superan a los normales por un margen considerable, lo que refuerza la percepción común de que los legendarios son más poderosos en términos de estadísticas totales.")
 
 # --- Indicador verde/rojo debajo de la diferencia ---
     if len(filtrados) > 0 and diferencia != 0:
@@ -66,11 +69,33 @@ with tab1:
         col3.metric("Diferencia", f"{diferencia:.1f} ({porcentaje:+.1f}%)", delta=delta_texto)
     else:
         col3.metric("Diferencia", "Sin datos")
+        
 #-----------------distribución del poder total----------------
+    
     st.subheader("📦 Distribución del poder total")
+    
     if len(filtrados) > 0:
-        fig_caja = px.box(filtrados, x="Is_Legendary", y="Total_Stats", color="Is_Legendary", points="all", title="Total_Stats: Legendarios vs Normales")
-        st.plotly_chart(fig_caja, use_container_width=True)
+        #creo dos columnas para mostrar los gráficos lado a lado
+        col_izq,col_der=st.columns(2)
+        
+        with col_izq:
+            legendarios_df = filtrados[filtrados['Is_Legendary']==True]
+            if len(legendarios_df) > 0:
+                fig_leg=px.box(legendarios_df,y="Total_Stats",title="Legendarios",points="all",color_discrete_sequence=["darkblue"])
+                st.plotly_chart(fig_leg, use_container_width=True)
+            else:
+                st.info("No hay legendarios con estos filtros.")    
+        with col_der:
+             normales_df = filtrados[filtrados['Is_Legendary']==False]
+             if len(normales_df) > 0:
+                fig_norm=px.box(normales_df,y="Total_Stats",title="Normales",points="all",color_discrete_sequence=["lightgray"])
+                st.plotly_chart(fig_norm, use_container_width=True)
+             else:
+                st.info("No hay normales con estos filtros.")
+    else:
+        st.warning("No hay datos disponibles para mostrar.")
+        
+        
 #-----------------grafico:fuerza media por tipo----------------
     st.subheader("🌿 Fuerza media por tipo elemental")
     if len(filtrados) > 0:
@@ -79,7 +104,6 @@ with tab1:
                          color="Total_Stats", color_continuous_scale="Viridis")
         fig_tipo.update_layout(yaxis={'categoryorder':'total ascending'})
         st.plotly_chart(fig_tipo, use_container_width=True)
-
 
 #--------------------------------------------------------------------------
 #-----------------------------segunda pestaña-----------------------------
@@ -100,10 +124,13 @@ with tab2:
         st.metric("porcentaje de normales con velocidad mayor o igual a la mediana legendaria", f"{porc_rapidos:.1f}%")
         fig_vel = px.scatter(filtrados, x="Speed", y="Total_Stats", color="Is_Legendary",hover_data=["Name"], title="Velocidad vs Poder Total")
         st.plotly_chart(fig_vel, use_container_width=True)
+        
+        st.markdown("📌 Conclusión: Aunque los Pokémon legendarios tienden a ser más rápidos en promedio, existe un porcentaje significativo de Pokémon normales que igualan o superan la velocidad de la mediana legendaria. Esto indica que no exclusivamente de los legendarios siempre son mas fuertes y que algunos Pokémon normales pueden ser sorprendentemente rápidos.")
+        
         st.subheader("📊 Top 10 normales más rápidos")
         top10 = normales_rapidos.nlargest(10, 'Speed')[['Name', 'Type_1', 'Speed', 'Total_Stats']]
         st.dataframe(top10, use_container_width=True)
-
+        st.markdown("📌 Conclusión clave: Entre los Pokémon normales, hay varios que alcanzan velocidades impresionantes, con algunos superando la mediana de los legendarios como \"Ninjask\" que es el más rapido de los no legendario, aunque en general, los legendarios siguen dominando en términos de velocidad media.")
 #--------------------------------------------------------------------------
 #-----------------------------tercera pestaña-----------------------------
 #--------------------------------------------------------------------------
@@ -117,9 +144,11 @@ with tab3:
 
     # Gráfico de barras agrupadas
     fig_bar = px.bar(type_stats, x="Tipo", y="Fuerza_Media", color="Legendario", #crea un gráfico de barras donde el eje x es el tipo, el eje y es la fuerza media, y las barras están coloreadas por condición legendaria
-                 barmode="group", title="Fuerza media por tipo y condición legendaria") #configura el modo de las barras para que estén agrupadas y establece el título del gráfico
+                 barmode="group", title="Comparativa de Fuerza total", subtitle="Legendarios vs No Legendarios") #configura el modo de las barras para que estén agrupadas y establece el título del gráfico
     st.plotly_chart(fig_bar, use_container_width=True) # muestra el gráfico en la aplicación de Streamlit, ajustando su ancho al contenedor disponible
-
+    
+    st.markdown("📌 Conclusión: En casi todos los tipos elementales, los Pokémon legendarios tienen una fuerza media significativamente mayor que los no legendarios. Esto sugiere que ser legendario es un factor más determinante para la fuerza total que el tipo elemental en sí. Sin embargo, algunos tipos como Dragón y Acero destacan por tener valores altos incluso entre los no legendarios, aunque aún así no alcanzan a sus contrapartes legendarias.")
+    
     st.subheader("📊 datos") #subtítulo para la sección de la tabl  a
     # Mostrar tabla estilo PDF
     st.dataframe(type_stats.sort_values("Fuerza_Media", ascending=False),) #muestra la tabla con la fuerza media por tipo y condición legendaria, ordenada de mayor a menor fuerza media
